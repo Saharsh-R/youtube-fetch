cf32c09537e06c09455438668dfa499999f8b0f6
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2021-08-10 23:03:02 +0530

    remove ignore file

--- src/components/key.ts
+++
@@ -1,7 +0,0 @@
-//project 34425
-// export const APIKEY = 'AIzaSyAQ_pgHZaIm6gkt5lGfyDZp2JXLhHsGFDQ'
-// export const CLIENTID = '978011831169-vopptu7tekbs5olhiib0k0fol5458gg7.apps.googleusercontent.com'
-
-// project TubeYouTube
-export const APIKEY = "AIzaSyAeJFCX3t7dbws-yaMorkENZ76zwa-97_E";
-export const CLIENTID = "871824101659-85ob0iimhfmgpjk3gvvvshnuvseo06f4.apps.googleusercontent.com";
